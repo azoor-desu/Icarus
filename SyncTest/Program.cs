e3599e310faaabf9b8fb7094ed17ce25d51fe4d8
azoor-guy <31021840+azoor-guy@users.noreply.github.com>
2021-12-24 16:48:03 +0800

    Fixed missing .dll and rearranged where libraries are

--- SyncTest/Program.cs
+++ SyncTest/Program.cs
@@ -1,19 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace SyncTest {
 	class Program {
 		static void Main(string[] args) {
 			Console.WriteLine("Hello World!");
 		}
+
+		enum ChangeType { Delete, Add, Rename, MetaChange }
+
+		const string localFolder = @"songs\";
+		const string serverFolder = @"Z:\TestSync";
+
+		// On first start: Look at server folder and see if it has been init'd yet.
+		// 1. is the .data folder there? is the .sync folder there? Is the .data folder parsable?
+		// if any of thsoe are NO, REBUILD.
+
+		//Reading File
+
+		//Writing File
+
+		//Sync botan
+
+
+		#region NTFS Unique Identifier Getter
+		//See: https://stackoverflow.com/questions/1866454/unique-file-identifier-in-windows
+		//Things to note: ID will never change on NTFS systems, unless deleted.
+		//If moved from drive to drive, ID will change. If file is deleted and replaced, ID will change.
+		//If used on FAT system, ID MAY change over time due to how the system stores bytes.
+		//So far, not sure if ID will change on ext4 on linux.
+
+		//FOR USE ON LOCAL SYSTEM ONLY. so the ext4 problem dosen't matter.
+
+		[DllImport("kernel32.dll")]
+		public static extern bool GetFileInformationByHandle(IntPtr hFile, out BY_HANDLE_FILE_INFORMATION lpFileInformation);
+
+		public struct BY_HANDLE_FILE_INFORMATION {
+			public uint FileAttributes;
+			public System.Runtime.InteropServices.ComTypes.FILETIME CreationTime;
+			public System.Runtime.InteropServices.ComTypes.FILETIME LastAccessTime;
+			public System.Runtime.InteropServices.ComTypes.FILETIME LastWriteTime;
+			public uint VolumeSerialNumber;
+			public uint FileSizeHigh;
+			public uint FileSizeLow;
+			public uint NumberOfLinks;
+			public uint FileIndexHigh;
+			public uint FileIndexLow;
+		}
+
+		public static string GetUniqueFileID(string path) {
+			BY_HANDLE_FILE_INFORMATION objectFileInfo = new BY_HANDLE_FILE_INFORMATION();
+
+			FileInfo fi = new FileInfo(path);
+			FileStream fs = fi.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+#pragma warning disable CS0618 // Type or member is obsolete
+			GetFileInformationByHandle(fs.Handle, out objectFileInfo);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+			fs.Close();
+
+			ulong fileIndex = ((ulong)objectFileInfo.FileIndexHigh << 32) + (ulong)objectFileInfo.FileIndexLow;
+
+			return fileIndex.ToString();
+
+		}
+		#endregion
 	}
-
-
-	//Reading File
-
-
-	//Writing File
-
-	//Sync botan
-
-
 }
